bfcf2b75579402b258dde4c0a190ddc5afeb35a3
quintuskilbourn <quintuskilbourn@gmail.com>
2020-04-08 14:50:27 +0800

    auto commit

--- remote.py
+++ remote.py
@@ -3,10 +3,10 @@ import networkx as nx
 import pickle
 res = []
 for p in [0.01,0.3,0.5,0.7]:
-
-    G = nx.watts_strogatz_graph(200,6,p)
-    test = mq.Kmed(G,3)
-    res.append(test.find_central_supernode())
+    for n in [150,200,250]:
+        G = nx.watts_strogatz_graph(200,6,p)
+        test = mq.Kmed(G,3)
+        res.append(test.find_central_supernode())
 
 with open("result.pkl",'wb') as f:
     pickle.dump(res,f)

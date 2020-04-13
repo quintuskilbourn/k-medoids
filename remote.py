6c5a45a10444ad4a8d5a23c7d4b9cdd23ae2bf18
quintuskilbourn <quintuskilbourn@gmail.com>
2020-04-13 19:28:29 +0800

    auto commit

--- remote.py
+++ remote.py
@@ -3,7 +3,7 @@ import networkx as nx
 import pickle
 import datetime
 res = []
-for p in [0.3]:
+for p in [0.3,0.3,0.7,0.7,0.7]:
     for n in [300,25,50,75,100,125,150,175,200,250,275]:
         G = nx.watts_strogatz_graph(n,6,p)
         test = mq.Kmed(G,3)

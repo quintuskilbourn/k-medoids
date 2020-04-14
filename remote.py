2038e1a95825c37b37d260edeae14e147e858c4f
quintuskilbourn <quintuskilbourn@gmail.com>
2020-04-14 19:56:15 +0800

    auto commit

--- remote.py
+++ remote.py
@@ -4,7 +4,7 @@ import pickle
 import datetime
 res = []
 for p in [0.3]:#[0.3,0.3,0.7,0.7,0.7]:
-    for n in [25,50,75,100,125,150,175,200,250]: #[275,300]
+    for n in [275,300]: #[25,50,75,100,125,150,175,200,250]
         G = nx.watts_strogatz_graph(n,6,p)
         test = mq.Kmed(G,3)
         res.append((p,test.find_central_supernode()))

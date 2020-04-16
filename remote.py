d3ff6798554659d36f4512defc52de8ac2e1f67e
quintuskilbourn <quintuskilbourn@gmail.com>
2020-04-16 23:15:49 +0800

    auto commit

--- remote.py
+++ remote.py
@@ -5,7 +5,7 @@ import datetime
 res = []
 file = "watts_"+str(int(datetime.datetime.now().timestamp()))+".pkl"
 for k in [2,3]:#4
-	for p in [0.03]:#[0.3,0.3,0.7,0.7,0.7]:
+	for p in [0.03,0.1,0.7]:#[0.3,0.3,0.7,0.7,0.7]:
 	    for n in [25,50,75,100,125,150,175,200,225,250]: #[275,300]
 	        G = nx.watts_strogatz_graph(n,6,p)
 	        test = mq.Kmed(G,k)
